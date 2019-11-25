558efd1c24e18f53a27242777be8a1c37abbfcf8
Edmund Lai <edmund.lai.07@gmail.com>
2019-11-24 19:03:32 -0800

    Added standard look to blog, removed boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Edmund's Blog</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/bio">Bio</Link>
+              </li>
+              <li>
+                <Link to="/portfolio">Portfolio</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <div className="App-body">
+          <Switch>
+            <Route path="/bio">
+              <Bio />
+            </Route>
+            <Route path="/portfolio">
+              <Portfolio />
+            </Route>
+          </Switch>
+        </div>
+
+        <div className="App-footer">
+          <p>Footer content</p>
+        </div>
+      </div>
+    </Router>
   );
+}
+
+class Bio extends React.Component {
+  render() {
+    return(
+      <div className="Bio">
+        Here is my bio section.
+      </div>
+    );
+  }
+}
+
+class Portfolio extends React.Component {
+  render() {
+    return(
+      <div className="Portfolio">
+        Here is my portfolio.
+      </div>
+    );
+  }
 }
 
 export default App;
