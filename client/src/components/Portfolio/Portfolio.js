09331f7a078a905220184239e7dd6130618ede2c
Edmund Lai <edmund.lai.07@gmail.com>
2019-11-26 00:55:37 -0800

    Added content to portfolio page.

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -1,23 +1,38 @@
 import React from 'react';
+import ProjectCard from '../ProjectCard/ProjectCard';
+import './Portfolio.css';
 
 class Portfolio extends React.Component {
   render() {
+    let desc1 =
+    <>
+      <p>Website dedicated to providing useful information for each game to League of Legends players looking to analyze their gameplay.</p>
+      <p>Provides important stats such as Kills, Deaths, Assists, and gold and experience earned to analyze overall performance.</p>
+      <p>Developed using React and Node.JS.</p>
+    </>;
+    let desc2 = 
+    <>
+      <p>Application developed to monitor latency to League of Legends servers outside of game, so that players can check if their internet connection is stable and won't impair their ingame performance.</p>
+      <p>Uses built in TCP/IP ping command in operating system to connect to the server IP address to check connection quality.</p>
+      <p>Developed using Python and Tkinter.</p>
+    </>
     return(
       <div className="Portfolio">
         <h2>
           Portfolio
         </h2>
-        <ul>
-          <li>
-            <a href="https://lol-stats-analysis.herokuapp.com/">League of Legends Stats App</a>
-          </li>
-          <li>
-            <a href="https://github.com/EdmundLai/League-of-Legends-Ping-App">League of Legends Ping Checker</a>
-          </li>
-          <li>
-            <a href="https://github.com/EdmundLai/react-blog">Sample Food Blog</a>
-          </li>
-        </ul>
+        <ProjectCard 
+          imgsrc="/portfolio/lolstats2.png"
+          title="League of Legends Stats App"
+          description={desc1}
+          projLink="https://lol-stats-analysis.herokuapp.com/"
+        />
+        <ProjectCard
+          imgsrc="/portfolio/ping_app_demo.png"
+          title="League of Legends Ping Checker"
+          description={desc2}
+          projLink="https://github.com/EdmundLai/League-of-Legends-Ping-App/"
+        />
       </div>
     );
   }
---
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ProjectCard.css';
+
+class ProjectCard extends React.Component {
+  render() {
+    return(
+      <div className="ProjectCard">
+        <div className="ProjectImage">
+          <a href={this.props.projLink}>
+            <img src={this.props.imgsrc} alt="Champion Stats Card"/>
+          </a>
+        </div>
+        <div className="ProjectDescription">
+          <h3>{this.props.title}</h3>
+          <div className="DescriptionBody">
+            {this.props.description}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProjectCard;
