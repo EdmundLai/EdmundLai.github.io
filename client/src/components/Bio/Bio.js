e412e87baef69d2b0ab0f8ad2d5b1eb375e4f02e
Edmund Lai <edmund.lai.07@gmail.com>
2019-12-09 16:32:03 -0800

    made website mobile friendly, changed index.html metadata

--- client/src/components/Bio/Bio.js
+++ client/src/components/Bio/Bio.js
@@ -2,26 +2,80 @@ import React from 'react';
 import './Bio.css';
 
 class Bio extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      windowWidth: window.innerWidth
+    }
+
+    this.handleWindowResize = this.handleWindowResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize);
+  }
+
+  handleWindowResize() {
+    this.setState({
+      windowWidth: window.innerWidth
+    });
+  }
+
   render() {
-    return(
-      <div className="Bio">
-        <div className="BioText">
-          <p>Hi, I'm Edmund!</p>
-          <p>
-            I'm passionate about creating code that helps make people's lives easier.
-          </p>
-          <p>
-            I'm a web developer, musician, and always curious to learn more about the world we live in.
-          </p>
-          <p>
-            I'm also interested in game development and games in general, so I enjoy making projects that can help people improve their gameplay or tilt a little less.
-          </p>
+    let isMobile = this.state.windowWidth < 500 ? true : false;
+
+    if(isMobile) {
+      return(
+        <div className="Bio">
+          <h2>About Me</h2>
+          <div className="BioContainerMobile">
+            <div className="BioImg">
+              <img src="/edmund.jpg" alt="Edmund Lai"/>
+            </div>
+            <div className="BioText">
+              <p>Hi, I'm Edmund!</p>
+              <p>
+                I'm passionate about creating code that helps make people's lives easier.
+              </p>
+              <p>
+                I'm a web developer, musician, and always curious to learn more about the world we live in.
+              </p>
+              <p>
+                I'm also interested in game development and games in general, so I enjoy making projects that can help people improve their gameplay or tilt a little less.
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="BioImg">
-          <img src="/edmund.jpg" alt="Edmund Lai"/>
+      );
+    } else {
+      return(
+        <div className="Bio">
+          <h2>About Me</h2>
+          <div className="BioContainer">
+            <div className="BioText">
+              <p>Hi, I'm Edmund!</p>
+              <p>
+                I'm passionate about creating code that helps make people's lives easier.
+              </p>
+              <p>
+                I'm a web developer, musician, and always curious to learn more about the world we live in.
+              </p>
+              <p>
+                I'm also interested in game development and games in general, so I enjoy making projects that can help people improve their gameplay or tilt a little less.
+              </p>
+            </div>
+            <div className="BioImg">
+              <img src="/edmund.jpg" alt="Edmund Lai"/>
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
--- client/src/components/ProjectCard/ProjectCard.js
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -2,22 +2,65 @@ import React from 'react';
 import './ProjectCard.css';
 
 class ProjectCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      windowWidth: window.innerWidth
+    }
+
+    this.handleWindowResize = this.handleWindowResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize);
+  }
+
+  handleWindowResize() {
+    this.setState({
+      windowWidth: window.innerWidth
+    });
+  }
+
   render() {
-    return(
-      <div className="ProjectCard">
-        <div className="ProjectImage">
-          <a href={this.props.projLink}>
-            <img src={this.props.imgsrc} alt={this.props.title}/>
-          </a>
-        </div>
-        <div className="ProjectDescription">
+
+    let isMobile = this.state.windowWidth < 500 ? true : false;
+
+    if(isMobile) {
+      return(
+        <div className="ProjectCardMobile">
           <h3>{this.props.title}</h3>
+          <div className="ProjectImage">
+            <a href={this.props.projLink}>
+              <img src={this.props.imgsrc} alt={this.props.title}/>
+            </a>
+          </div>
           <div className="DescriptionBody">
             {this.props.description}
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return(
+        <div className="ProjectCard">
+          <div className="ProjectImage">
+            <a href={this.props.projLink}>
+              <img src={this.props.imgsrc} alt={this.props.title}/>
+            </a>
+          </div>
+          <div className="ProjectDescription">
+            <h3>{this.props.title}</h3>
+            <div className="DescriptionBody">
+              {this.props.description}
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
